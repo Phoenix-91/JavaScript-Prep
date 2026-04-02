3ca3774f6a5525319c78bc020df133dbb1a4cde0
Paramveer R <paramveerpc2211@gmail.com>
2026-04-02 19:00:27 +0530

    anthor one

---
+++ objectEmpty.js
@@ -0,0 +1,14 @@
+// // checking object empty or not
+// const userObj ={
+//     id: 1,
+//     name: "saurav",
+//     age: 22,
+//     course: "MCA"
+// }
+// if(Object.keys(userObj).length === 0){  
+//     console.log("object is empty");
+// }
+// else{
+//     console.log("object is not empty");
+//     console.log(`here are the details in userObj ${userObj.name}, ${userObj.age}, ${userObj.course} `);
+// }
